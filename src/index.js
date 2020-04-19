c078a7cae7b81210c5200fcc1e92db96e89f876a
Igor <igor.s@xola.com>
2020-04-19 18:05:28 +0200

    Rename the bundle for consistency

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { Bundle } from "kolaz/Component/Bundle";
 import { WinstonService } from "./services/WinstonService";
 
-export class WinstonLogger extends Bundle {
+export class WinstonJsBundle extends Bundle {
     configureContainer(container) {
         container.add("log", WinstonService, ["params"]);
     }
